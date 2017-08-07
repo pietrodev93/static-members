fc53a8f703e3f2b818f17c62305d514e9d85164a
Pietro Giordano <pietrogiordano93@gmail.com>
2017-08-07 12:48:14 +0200

    fixed form

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BMembriStatici = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BMembriStatici
+            // 
+            this.BMembriStatici.Location = new System.Drawing.Point(12, 12);
+            this.BMembriStatici.Name = "BMembriStatici";
+            this.BMembriStatici.Size = new System.Drawing.Size(75, 23);
+            this.BMembriStatici.TabIndex = 0;
+            this.BMembriStatici.Text = "Mostra punti";
+            this.BMembriStatici.UseVisualStyleBackColor = true;
+            this.BMembriStatici.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Decrementa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BMembriStatici);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BMembriStatici;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+        class Punti
+        {
+            static int numeroPunti = 0;
+
+            public static void SituazionePunti()
+            {
+                MessageBox.Show("Hai accumulato: " + numeroPunti.ToString());
+            }
+
+            public Punti()
+            {
+                ++numeroPunti;
+            }
+
+            public static void ResetContuer()
+            {
+                numeroPunti = 0;
+                MessageBox.Show(numeroPunti.ToString());
+            }
+        }
+
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Punti p1, p2, p3;
+
+            Punti.SituazionePunti();
+            p1 = new Punti();
+            Punti.SituazionePunti();
+
+            p2 = new Punti();
+            Punti.SituazionePunti();
+
+            
+            Punti.ResetContuer();
+
+        }
+
+ 
+    }
+}
